92fd9dcf7ec9ecd525979fa0ae1fd0521491f07b
cloudcastaway <66567923+cloudcastaway@users.noreply.github.com>
2023-10-23 02:02:25 -0300

    Add while loop for repeatability

--- script.py
+++ script.py
@@ -1,23 +1,31 @@
 from convert import Convert
+from intro import logo, greetings
 
 if __name__ == '__main__':
 
-    selection = input("Would you like to encrypt or decrypt a string? (e/d)").lower()
-    while selection != "e" and selection != "d":
-        selection = input("Enter \"e\" for encrypting or \"d\" for decrypting: ").lower()
+    print(logo)
+    print(greetings + "\n")
 
-    convert_string = Convert()
-    invalid_char = True
-    while invalid_char:
-        string = input(f"Type in a word or sentence to {convert_string.get_method(selection)}: ").lower()
-        for char in string:
-            if char not in convert_string.get_chart():
-                print(f"You typed in an invalid character: {char}")
-                break
-        invalid_char = False
+    proceed = "y"
+    while proceed == "y":
+        selection = input("Encrypt or decrypt a string? (e/d): ").lower()
+        while selection != "e" and selection != "d":
+            selection = input("Enter \"e\" for encrypting or \"d\" for decrypting: ").lower()
 
-    if selection == "e":
-        print(convert_string.encrypt(string))
-    else:
-        print(convert_string.decrypt(string))
+        convert_string = Convert()
+        invalid_char = True
+        while invalid_char:
+            string = input(f"Type in a word or sentence to {convert_string.get_method(selection)}: ").lower()
+            for char in string:
+                if char not in convert_string.get_chart():
+                    print(f"You typed in an invalid character: {char}")
+                    break
+            invalid_char = False
+
+        print(f"{convert_string.get_method(selection)}ed string: ".capitalize(), end = "")
+        if selection == "e":
+            print(convert_string.encrypt(string))
+        else:
+            print(convert_string.decrypt(string))
         
+        proceed = input("\nWould you like to convert another string? (y/n): ").lower()
